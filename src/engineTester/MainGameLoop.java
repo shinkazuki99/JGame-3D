0e65c84489cc54ff309e327c6cc113e7b91c358c
shinkazuki99 <shinkazuki99@gmail.com>
2016-05-30 17:29:28 -0500

    render efficient

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -18,17 +18,17 @@ public class MainGameLoop {
 		
 		float[] vertices = { 
 				//Left Bottom Triangle
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f, 
-				0.5f, -0.5f, 0f, 
-				
-				//Right top triangle
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f, 
-				-0.5f, 0.5f, 0f
+				-0.5f, 0.5f, 0,   //V0
+				-0.5f, -0.5f, 0,  //V1
+				0.5f, -0.5f, 0,   //V2
+				0.5f,0.5f, 0      //V3
+		};
+		int[] indices = {
+				0,1,3, // top left triangle
+				3,1,2 //bottom right triangle
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		RawModel model = loader.loadToVAO(vertices,indices);
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
