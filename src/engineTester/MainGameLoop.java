55da191c121844ac65f30fc8a081b5786ec23fee
shinkazuki99 <shinkazuki99@gmail.com>
2016-05-30 19:14:16 -0500

    shade

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -15,6 +16,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] vertices = { 
 				//Left Bottom Triangle
@@ -32,11 +34,13 @@ public class MainGameLoop {
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
-			//game logic
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
